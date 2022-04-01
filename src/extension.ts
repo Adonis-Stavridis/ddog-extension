930859bd1e29ab8a489a8eba84d0599873d5399e
Adonis Stavridis <adonis.stavridis@gmail.com>
2022-04-02 01:41:35 +0200

    🚀 display $DDOG in statusbar

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,61 @@
 import * as vscode from 'vscode';
+import axios from 'axios';
+
+const API_TOKEN = 'c93h21aad3icjtmbvd70';
+const SYMBOL = 'DDOG';
+const TIMEOUT_TIME = 120; // 2 minutes
+
+const UP_COLOR = 'mediumseagreen';
+const DOWN_COLOR = 'tomato';
+
+type ResponseData = {
+  c: number;
+  d: number;
+  dp: number;
+  h: number;
+  l: number;
+  o: number;
+  pc: number;
+  t: number;
+};
+
+const displayDDOG = (item: vscode.StatusBarItem, data: ResponseData) => {
+  const current = data.c;
+  const change = data.d;
+  const positive = change >= 0;
+
+  item.text = `\$${SYMBOL}: ${current} |${
+    positive ? '$(arrow-small-up)' : '$(arrow-small-down)'
+  }${Math.abs(change).toFixed(2)}`;
+
+  item.color = positive ? UP_COLOR : DOWN_COLOR;
+
+  item.show();
+};
+
+const getDDOG = (item: vscode.StatusBarItem) => {
+  axios
+    .get('https://finnhub.io/api/v1/quote', {
+      params: {
+        symbol: SYMBOL,
+        token: API_TOKEN
+      }
+    })
+    .then(response => {
+      console.log(response);
+      displayDDOG(item, response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
 
 export const activate = () => {
-    const ddogViewer = vscode.window.createStatusBarItem(
-        vscode.StatusBarAlignment.Right
-    );
+  const ddogItem = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right
+  );
+  ddogItem.tooltip = `🚀 To the moon! 💎🙌`;
 
-    ddogViewer.text = '$DDOG';
-    ddogViewer.color = 'tomato';
-
-    ddogViewer.show();
+  getDDOG(ddogItem);
+  setInterval(() => getDDOG(ddogItem), TIMEOUT_TIME * 1000);
 };
